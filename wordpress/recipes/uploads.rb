df3f1ce8e99328c3cb11e7d78bc7101b3b224081
Leonardo Borba <lmborba@gmail.com>
2016-02-13 20:05:02 +0100

    Fixed wp-config.php

--- wordpress/recipes/uploads.rb
+++ wordpress/recipes/uploads.rb
@@ -2,4 +2,7 @@ node[:deploy].each do |app_name, deploy|
   link "#{deploy[:deploy_to]}/current/wp-content/uploads" do
     to "#{deploy[:deploy_to]}/uploads"
   end
+  link "#{deploy[:deploy_to]}/current/wp-config.php" do
+    to "#{deploy[:deploy_to]}/wp-config.php"
+  end
 end
